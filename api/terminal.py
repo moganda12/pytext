d3e91c522305f3da4d5d02dffde485d77c23385e
moganda12 <115957608+moganda12@users.noreply.github.com>
2023-08-27 23:02:18 +0000

    THE BEGINNIG

---
+++ api/terminal.py
@@ -0,0 +1,10 @@
+
+
+class TextInterface():
+    """
+    Text interface for engine API.
+    """
+    def __init__(self):
+        pass
+    def print(self, text):
+        print(text)
---
+++ main/main.py
@@ -0,0 +1,10 @@
+import time
+def wait(timee): 
+    """
+        wait for timee milliseconds
+    
+        Args: 
+        timee (int): milliseconds to wait 
+    """ 
+    timee = timee * 1000 
+    time.sleep(timee)
