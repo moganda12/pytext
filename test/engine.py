ba4e9e57e68b66977f38d028b932d4f2bb133136
moganda12 <115957608+moganda12@users.noreply.github.com>
2023-09-05 18:18:00 +0000

    HACK EDITOIN COMPLETEEEEEEEEEE

--- test/engine.py
+++ test/engine.py
@@ -1,3 +1,5 @@
+from colorama import Fore as col
+from colorama import Style
 import time
 
 def wait(timee):
@@ -31,6 +33,14 @@ class Commanager():
         self.commands.append(Command(command, comargs))
 
     def procinput(self, input):
+        """process input
+
+        Args:
+            input (str): user's input
+
+        Returns:
+            dict: command arguments
+        """
         output = {}
         comtoks = input.split(' ')
         for i in enumerate(self.commands):
@@ -38,4 +48,35 @@ class Commanager():
                 comtoks.pop(0)
                 for j in enumerate(comtoks):
                     output[i[1].comargs[j[0]]] = comtoks[j[0]]
-        return output
+                return i[1].command, output
+        print(col.RED + "unknown command: " + comtoks[0])
+        print(Style.RESET_ALL, end='\n')
+        return ''
+
+class TextManager():
+    """Manager for text printing."""
+    def __init__(self):
+        pass
+
+    def print(self, text):
+        """print the given text
+
+        Args:
+            text (str): text to print
+        """
+        print(text)
+
+    def printslow(self, text, delay = 0.2):
+        """prints characters of text one at a time
+
+        Args:
+            text (str): text to print
+            delay (float, optional): delay per charachter in seconds. Defaults to 0.2.
+        """
+        text = [*str(text)]
+
+        for i in enumerate(text):
+            print(i[1], end='', flush=True)
+            time.sleep(delay)
+        
+        print('')
--- test/test.py
+++ test/test.py
@@ -1,7 +1,8 @@
 import engine
 
 commanager = engine.Commanager()
+textmanager = engine.TextManager()
 
 commanager.addcom('test', ('mmmmmmmm', 'yum'))
 
-print(commanager.procinput(input('command:')))
+textmanager.printslow(commanager.procinput('test mmmmmmmmm yum'))
